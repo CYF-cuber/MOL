1776fd0bb8db2a7a3ef8041b0975450a19cf439d
CYF-cuber <85044231+CYF-cuber@users.noreply.github.com>
2024-10-14 22:15:26 +0800

    Update metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -6,22 +6,23 @@ def calculate_metrics(confusion_matrix):
     # Accuracy (ACC)
     total_samples = np.sum(confusion_matrix)
     correct_predictions = np.trace(confusion_matrix)
-    accuracy = correct_predictions / total_samples
+    accuracy = correct_predictions / total_samples if total_samples != 0 else 0
     
     # Weighted Average Recall (WAR)
-    weighted_recall = np.sum(np.diag(confusion_matrix) / np.sum(confusion_matrix, axis=1) * np.sum(confusion_matrix, axis=0)) / total_samples
+    class_sums = np.sum(confusion_matrix, axis=1)
+    recall_per_class = np.where(class_sums != 0, np.diag(confusion_matrix) / class_sums, 0)
+    weighted_recall = np.sum(recall_per_class * class_sums) / total_samples if total_samples != 0 else 0
     
     # Unweighted Average Recall (UAR)
-    recall_per_class = np.diag(confusion_matrix) / np.sum(confusion_matrix, axis=1)
-    unweighted_recall = np.mean(recall_per_class)
+    unweighted_recall = np.mean(recall_per_class) if recall_per_class.size > 0 else 0
     
     # Weighted F1 Score (WF1)
-    precision = np.diag(confusion_matrix) / np.sum(confusion_matrix, axis=0)
-    recall = np.diag(confusion_matrix) / np.sum(confusion_matrix, axis=1)
-    f1_score_per_class = 2 * precision * recall / (precision + recall)
-    weighted_f1_score = np.sum(f1_score_per_class * np.sum(confusion_matrix, axis=1) / total_samples)
+    precision_sums = np.sum(confusion_matrix, axis=0)
+    precision_per_class = np.where(precision_sums != 0, np.diag(confusion_matrix) / precision_sums, 0)
+    f1_score_per_class = np.where((precision_per_class + recall_per_class) != 0, 2 * precision_per_class * recall_per_class / (precision_per_class + recall_per_class), 0)
+    weighted_f1_score = np.sum(f1_score_per_class * class_sums) / total_samples if total_samples != 0 else 0
     
     # Unweighted F1 Score (UF1)
-    unweighted_f1_score = np.mean(f1_score_per_class)
+    unweighted_f1_score = np.mean(f1_score_per_class) if f1_score_per_class.size > 0 else 0
     
     return accuracy, weighted_recall, unweighted_recall, weighted_f1_score, unweighted_f1_score
